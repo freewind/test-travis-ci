f68d47d81c5c465309daf16cdb1f3d24f4c2b52e
Freewind <pli@thoughtworks.com>
2014-07-05 20:50:49 +0800

    Still not work :((

--- scripts/test-travis.sh
+++ scripts/test-travis.sh
@@ -6,7 +6,6 @@ cat .build_version
 
 git add .build_version
 
-git remote -v
 git status
 
 git config --global user.email "travis-ci@travis-ci.org"
@@ -17,5 +16,8 @@ git --no-pager log --stat -3
 
 git remote add token https://${GH_TOKEN}@github.com/freewind/test-travis-ci.git
 git push token master
+
+git remote -v
+git status
 
 echo done
