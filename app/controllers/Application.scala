db9f6959a8a63032b396651dbca5547d3e65ea87
Freewind <pli@thoughtworks.com>
2014-07-05 14:54:27 +0800

    Simplest solution for a json parse and render

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,18 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
+import play.api.libs.json.Json
 
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def hello = Action(parse.text) { request =>
+    val content = request.body
+    val name = Json.parse(content) \ "name"
+    Ok( s"""{$name:"welcome"}""").as("application/json")
   }
 
 }
--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,7 +1,8 @@
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
-
+import play.api.libs.json.Json
+import play.api.mvc.{AnyContentAsText, AnyContentAsJson}
 import play.api.test._
 import play.api.test.Helpers._
 
@@ -15,16 +16,28 @@ class ApplicationSpec extends Specification {
 
   "Application" should {
 
-    "send 404 on a bad request" in new WithApplication{
+    "send 404 on a bad request" in new WithApplication {
       route(FakeRequest(GET, "/boum")) must beNone
     }
 
-    "render the index page" in new WithApplication{
+    "render the index page" in new WithApplication {
       val home = route(FakeRequest(GET, "/")).get
 
       status(home) must equalTo(OK)
       contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
+      contentAsString(home) must contain("Your new application is ready.")
     }
   }
+
+  "Application" should {
+    "send a welcome json with the name I posted" in new WithApplication() {
+      val hello = route(FakeRequest(POST, "/hello", FakeHeaders(), AnyContentAsText("""{"name":"scala_workshop"}"""))).get
+
+      status(hello) must equalTo(OK)
+      contentType(hello) must beSome.which(_ == "application/json")
+      contentAsString(hello) must equalTo( """{"scala_workshop":"welcome"}""")
+    }
+  }
+
+
 }
