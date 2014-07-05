0092d086784acfc53d1a4c58d09b4722b6a626d5
Freewind <pli@thoughtworks.com>
2014-07-05 20:28:30 +0800

    Just some crazy code to test travis-ci 5

--- scripts/test-travis.sh
+++ scripts/test-travis.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 pwd
 
-git remote -v
-
-git status
-
-git log
-
 echo my_version > .build_version
 cat .build_version
 
 git add .build_version
+
+git remote -v
+git status
+
 git config --global.email "travis-ci@travis-ci.org"
 git config --global.username "travis-ci"
 git commit -m "update .build_version"
+
+git --no-pager log --stat
+
 git push origin master
 
 echo done
