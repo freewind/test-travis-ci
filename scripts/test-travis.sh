78892f8f792639f776a6f99e3eabd724609cd4df
Freewind <pli@thoughtworks.com>
2014-07-05 20:41:25 +0800

    Use token to push

--- scripts/test-travis.sh
+++ scripts/test-travis.sh
@@ -13,8 +13,9 @@ git config --global user.email "travis-ci@travis-ci.org"
 git config --global user.name "travis-ci"
 git commit -m "update .build_version"
 
-git --no-pager log --stat
+git --no-pager log --stat -3
 
-git push origin master
+git remote add token https://${GH_TOKEN}@github.com/freewind/test-travis-ci.git
+git push token master
 
 echo done
