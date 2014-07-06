db17906da96c10874d48fb759264c0920851c889
Freewind <pli@thoughtworks.com>
2014-07-06 14:53:28 +0800

    Fix nohup filepath

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup bin/test-travis-ci -Dhttp.port=9090 &
+nohup ./bin/test-travis-ci -Dhttp.port=9090 &> logs/nohup.log
 # You can provide all the -Dsettings you need to set for your application here :-)
