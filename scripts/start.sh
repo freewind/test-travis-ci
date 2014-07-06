409bf88b0baa1d233a6e417ba22016243465e4cd
Freewind <pli@thoughtworks.com>
2014-07-06 15:03:38 +0800

    Ignore the nohup log

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-nohup ./bin/test-travis-ci -Dhttp.port=9090 &> logs/nohup.log
+mkdir -p logs
+nohup ./bin/test-travis-ci -Dhttp.port=9090 >/dev/null 2>&1 &
 # You can provide all the -Dsettings you need to set for your application here :-)
